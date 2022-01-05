4bf6c79896cadbb54b820b480addabc21791b5fb
ogremagi4 <ogremagi9@gmail.com>
2022-01-05 18:29:07 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,12 +1,16 @@
-from threading import Thread
+from threading import Thread, Lock, Event
 from queue import Queue, Empty
 from websocket import create_connection
+from collections import ChainMap
 from loguru import logger
 import json
 import regex
 import string
 import random
+import traceback
 from requests import Session
+from tenacity import retry, stop_after_attempt, wait_fixed, retry_if_exception_type
+from requests.exceptions import HTTPError
 
 
 json_rgx = regex.compile(r'''
@@ -47,12 +51,12 @@ class TradingViewWsMessages:
         return message
 
     def resolve_symbol(self, chart_session_string, tradingview_symbol):#
-        symbol_param  = f'={json.dumps({"symbol":tradingview_symbol, "adjustment":"splits","session":"extended"})}'.replace('"','\"')
+        symbol_param  = f'={json.dumps({"symbol":tradingview_symbol, "adjustment":"splits"})}'.replace('"','\"')
         message = self.create_message('resolve_symbol', [chart_session_string, 'sds_sym_1', symbol_param])#TODO maybe you'd make symbol_{index} and use it somehow
         return message
     
     def create_series(self, chart_session_string, tradingview_timeframe, limit=5000):
-        message = self.create_message('create_series', [chart_session_string, "s1", "s1", "symbol_1", tradingview_timeframe, limit])
+        message = self.create_message('create_series', [chart_session_string, "sds_1", "s1", "sds_sym_1", tradingview_timeframe, 300, "12M"])#TODO map arguments insetad of hardcode
         return message
     
     def quote_fast_symbols(self, quote_session_string, tradingview_symbol):
@@ -68,7 +72,7 @@ class TradingViewWsMessages:
         return message
     
     def quote_add_symbols(self, quote_session_string, tradingview_symbol):
-        message = self.create_message('quote_add_symbols', [quote_session_string,tradingview_symbol, {'flags':["force_permission"]}])
+        message = self.create_message('quote_add_symbols', [quote_session_string,tradingview_symbol])
         return message
     
     
@@ -101,15 +105,17 @@ class WS:
             try:
                 msg = self.queue.get_nowait()
                 if msg:
-                    logger.debug(f'Msg from ws: {msg}')
+                    #logger.debug(f'Msg from ws: {msg}')
                     return msg
             except Empty:
                 return None
                 
         def run(self):
             while True:
-                self.queue.put( self.ws.recv() )
-
+                if self.ws.connected:
+                    self.queue.put( self.ws.recv() )
+                else:
+                    break
 
     class Sender(Thread):
         
@@ -117,7 +123,7 @@ class WS:
             super().__init__(daemon = True)
             self.queue = Queue()
             self.ws = ws
-
+            self.kill = Event()
             self.start()
 
         def send(self, item):
@@ -125,8 +131,9 @@ class WS:
 
 
         def run(self):
-            while True:
-                self.ws.send( self.queue.get(block = True) )
+            while not self.kill.is_set():
+                if not self.queue.empty():
+                    self.ws.send( self.queue.get_nowait())
 
 
     def __init__(self):
@@ -150,7 +157,8 @@ class TradingviewWsScraper:
     def _generate_session(self, prefix = 'qs_'):
         string_length=12
         letters = string.ascii_lowercase
-        random_string= ''.join(random.choice(letters) for i in range(string_length))
+        digits = string.digits
+        random_string= ''.join(random.choice(letters+digits) for i in range(string_length))
         return prefix + random_string
 
     def chart_session(self):
@@ -169,6 +177,7 @@ class TradingviewWsScraper:
         return ws
     
     def get_symbol(self, tradingview_symbol):
+        logger.debug(f'Trying to get {tradingview_symbol}')
         result = []
         quote_session_string = self.quote_session()
         ws = self._init_websocket_and_authorize()
@@ -179,32 +188,43 @@ class TradingviewWsScraper:
                     self.messages.quote_add_symbols(quote_session_string, tradingview_symbol),
                     self.messages.quote_fast_symbols(quote_session_string, tradingview_symbol)]
         for message in messages:
-            logger.debug(f'Sending {message}')
             ws.send(message)
         
         while True:
             resp = ws.get()
             if resp:
+                if resp == '~m~4~m~~h~1': #TODO less hardcode
+                    break
                 logger.debug(resp)
-                result.extend([json.loads(i) for i in json_rgx.findall(resp)])
+                result.extend([json.loads(i) for i in json_rgx.findall(resp.replace('\\"',''))])
             if (result or [{}])[-1].get('m') == 'quote_completed':
-                ws.ws.send_close()
+                try:
+                    ws.sender.kill.set()
+                    ws.ws.send_close()
+                except:
+                    logger.error(traceback.format_exc())
+                
                 return [i.get('p')[1].get('v') for i in result if (isinstance(i,dict) and isinstance(i.get('p',[[],[]])[1],dict))]
             
         
 
     def get_candles(self, tradingview_symbol, tradingview_timeframe):
-        #[time ohlc volume]
         result = []
-        chart_session_string = self.generate_session('cs_')
+        quote_session_string = self.quote_session()
+        chart_session_string = self.chart_session()
         ws = self._init_websocket_and_authorize()
         messages = [self.messages.set_auth_token(self.token), 
+                    self.messages.set_data_quality('low'),
+                    # self.messages.quote_create_session(quote_session_string), 
+                    #self.messages.quote_add_symbols(quote_session_string, tradingview_symbol),
+                    #self.messages.quote_fast_symbols(quote_session_string, tradingview_symbol),
                     self.messages.chart_create_session(chart_session_string), 
                     self.messages.resolve_symbol(chart_session_string, tradingview_symbol),
                     self.messages.create_series(chart_session_string, tradingview_timeframe)]
-        for message in messages:
-            logger.debug(f'Sending {message}')
+
+        for message in messages:#TODO wrap it somehow to a separate method sending and receiving messages
             ws.send(message)
+            logger.debug(message)
         while True:
             resp = ws.get()
             if resp:
@@ -212,7 +232,7 @@ class TradingviewWsScraper:
                 result.extend([json.loads(i) for i in json_rgx.findall(resp)])
             for response_message in result:
                 if response_message.get('m')=='timescale_update':
-                    candles = list(reversed([i.get('v') for i in next(i.get('s1') for i in response_message.get('p') if isinstance(i,dict)).get('s')]))
+                    candles = [i.get('v') for i in dict(ChainMap(*[i for i in response_message.get('p') if isinstance(i, dict)]))['sds_1']['s']]#TODO validate response somehow
                     ws.ws.send_close()
                     return candles
 
@@ -221,10 +241,16 @@ class TradingViewRestScraper:
     def __init__(self) -> None:
         self.headers = {
          #'Origin': 'https://ru.tradingview.com',
-         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36'
+         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36' #TODO dynamic user-agent
         }
 
         self.session = Session()
+
+    @retry(stop = stop_after_attempt(10), wait = wait_fixed(60), retry=retry_if_exception_type(HTTPError))
+    def get(self, endpoint, params={}, jdata={}, headers={}):
+        r = self.session.get(endpoint, params=params, json=jdata, headers={**self.session.headers, **headers})
+        r.raise_for_status()
+        return r.json()
     
     def search_symbol(self, symbol, **kwargs):
         params = {
@@ -234,5 +260,9 @@ class TradingViewRestScraper:
             'domain':'production',
             **kwargs
         }
-        response = self.session.get('https://symbol-search.tradingview.com/symbol_search/', params=params)
+        response = self.get('https://symbol-search.tradingview.com/symbol_search/', params=params) #TODO wrap urls
         return response
+    
+    def get_sector_symbols(self, sector):
+        response = self.get(f'https://scanner.tradingview.com/{sector}/scan') #TODO wrap urls
+        return [i.get('s') for i in response.get('data')]
